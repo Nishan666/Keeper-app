be4d7ab92681259492dfef4cca009823d25fde65
Nishan Kumar <nishankumar559@gmail.com>
2023-05-02 22:06:07 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "./Sub-components/Header";
+import Footer from "./Sub-components/Footer";
+import Notes from "./Sub-components/Notes";
+
+
+
+function App(){
+    return (
+        <div>
+            <Header></Header>
+            <Notes></Notes>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default App;
