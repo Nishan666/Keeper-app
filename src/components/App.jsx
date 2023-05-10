d419e149bc2426b7342712e9a0d191542d8c7926
Nishan Kumar <nishankumar559@gmail.com>
2023-05-10 14:14:53 +0530

    part 2 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,18 @@ import Header from "./Sub-components/Header";
 import Footer from "./Sub-components/Footer";
 import Notes from "./Sub-components/Notes";
 
+import notes from "../notes";
+
 
 
 function App(){
     return (
-        <div>
-            <Header></Header>
-            <Notes></Notes>
-            <Footer></Footer>
-        </div>
-    )
+      <div>
+        <Header></Header>
+        {notes.map(data =>( <Notes key={data.key} title={data.title} content={data.content} />)) }
+        <Footer></Footer>
+      </div>
+    );
 }
 
 export default App;
