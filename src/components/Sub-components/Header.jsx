5432a47dbf21e2bca1aa131348ccb31c5ad571d8
Nishan Kumar <nishankumar559@gmail.com>
2023-05-18 13:41:04 +0530

    og styling Done

--- src/components/Sub-components/Header.jsx
+++ src/components/Sub-components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import OfflineBoltIcon from "@mui/icons-material/OfflineBolt";
 
 function Header(){
     return (
       <header>
-        <h1>Keeper</h1>
+        <h1><OfflineBoltIcon/>Keeper</h1>
       </header>
     );
 }
