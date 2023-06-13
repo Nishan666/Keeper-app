6367506e5520034dad54760a8d8bafbf1e7a1bfa
Nishan Kumar <nishankumar559@gmail.com>
2023-06-13 23:57:29 +0530

    Updates

---
+++ precache-manifest.4b2dc08afb77ab78c3e10fffacfdf4a8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "43892b58c4c4319f584a",
+    "url": "/Keeper-app/static/js/main.43892b58.chunk.js"
+  },
+  {
+    "revision": "1ef9fa7c8982b5a0923a",
+    "url": "/Keeper-app/static/js/1.1ef9fa7c.chunk.js"
+  },
+  {
+    "revision": "8fd75a741da1a17237fa",
+    "url": "/Keeper-app/static/js/runtime~main.8fd75a74.js"
+  },
+  {
+    "revision": "504f166d90f16d082179d7a6b0547e02",
+    "url": "/Keeper-app/index.html"
+  }
+];
