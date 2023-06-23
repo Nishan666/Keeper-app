af612529200afb66391414b32888ffbe3948c992
nishan666 <nishankumar559@gmail.com>
2023-06-23 23:33:53 +0530

    Updates

---
+++ precache-manifest.bb4395eea7c63b5291b628496a767418.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "208f6cd96f26bcadf8a4",
+    "url": "/Keeper-app/static/css/main.720dd4ab.chunk.css"
+  },
+  {
+    "revision": "208f6cd96f26bcadf8a4",
+    "url": "/Keeper-app/static/js/main.309c04ff.chunk.js"
+  },
+  {
+    "revision": "4a2ccb5260b2071bb592",
+    "url": "/Keeper-app/static/js/runtime~main.054d6fcc.js"
+  },
+  {
+    "revision": "5eca3295773d43493b35",
+    "url": "/Keeper-app/static/js/2.aab29074.chunk.js"
+  },
+  {
+    "revision": "7d4be904f48578b7cf6ac5eff6509452",
+    "url": "/Keeper-app/index.html"
+  }
+];
